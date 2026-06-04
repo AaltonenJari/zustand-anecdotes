8689122820a1f486764d7a4083923b769bccac88
AaltonenJari <jaritapani.aaltonen@gmail.com>
2026-06-04 15:45:11 +0300

    Tehtävä 6.7 anekdootit, step6

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Filter from './components/Filter'
+import { useEffect } from 'react'
+import { useAnecdoteActions } from './store'
+import anecdoteService from './services/anecdotes'
 
 const App = () => {
+  const { initAnecdotes } = useAnecdoteActions()
+
+  useEffect(() => {
+    anecdoteService.getAll().then(anecdotes => initAnecdotes(anecdotes))
+  }, [initAnecdotes])
+
   return (
     <div>
       <Filter />
